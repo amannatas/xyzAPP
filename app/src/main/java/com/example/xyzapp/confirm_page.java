50f55db50fa7347c9e63382c76d722b0f31ee93e
Aman rathor <amanrathor7410@.gmail.com>
2025-05-31 19:40:26 +0530

    day3

--- app/src/main/java/com/example/xyzapp/confirm_page.java
+++ app/src/main/java/com/example/xyzapp/confirm_page.java
@@ -1,6 +1,9 @@
 package com.example.xyzapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,14 @@ public class confirm_page extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_confirm_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button user_signup = findViewById(R.id.user_signup);
+        user_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent inext = new Intent(confirm_page.this, home_page.class);
+                startActivity(inext);
+            }
         });
     }
 }
--- app/src/main/java/com/example/xyzapp/login_page.java
+++ app/src/main/java/com/example/xyzapp/login_page.java
@@ -1,6 +1,10 @@
 package com.example.xyzapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +19,30 @@ public class login_page extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Button user_login = findViewById(R.id.user_login);
+        TextView signup_page = findViewById(R.id.signup_page);
+        TextView forgot_btn = findViewById(R.id.forgot_btn);
+
+        user_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(login_page.this, home_page.class);
+                startActivity(intent);
+            }
+        });
+        signup_page.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(login_page.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        forgot_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(login_page.this, forgot_password.class);
+                startActivity(intent);
+            }
         });
     }
 }
